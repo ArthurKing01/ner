6a4dbcbb524f801aaad80fba809469b4eeca40e4
Arthur <1352854163@qq.com>
2021-08-27 16:56:22 +0800

    add tflite

--- h52pb.py
+++ h52pb.py
@@ -3,6 +3,6 @@
 import kashgari
 from kashgari.utils import convert_to_saved_model
 
-loaded_model = kashgari.utils.load_model('bilstm_per_ner.h5')
+loaded_model = kashgari.utils.load_model('per_ner.h5')
 
 convert_to_saved_model(loaded_model, 'models/')
