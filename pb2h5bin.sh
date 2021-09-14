bc240550a68f3bd0fdd5d603c8b91e066a85393d
arthurhuangkang <arthurhuangkang@didiglobal.com>
2021-09-14 11:50:20 +0800

    add test attension

--- pb2h5bin.sh
+++ pb2h5bin.sh
@@ -5,3 +5,10 @@ tensorflowjs_converter \
     --saved_model_tags=serve \
     /mobilenet/saved_model \
     /mobilenet/web_model
+
+    tensorflowjs_converter \            
+    --input_format=tf_saved_model \
+    --output_node_names='webModels' \
+    --saved_model_tags=serve \
+    ./models/1627553771 \
+    ./web_model/web_model
